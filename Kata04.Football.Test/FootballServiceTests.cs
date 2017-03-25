2474041a7af4f5f39fbc1fc2e94ad8fa1db001a6
Aaron Chancey <achancey@quicksolutions.com>
2017-03-25 19:59:05 -0400

    test for question 2

---
+++ Kata04.Football.Test/FootballServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata04.Football.Test
+{
+    [TestClass]
+    public class GivenAFootballService
+    {
+        [TestMethod]
+        public void ThenGetTeamWithSmallestScoringDifferenceReturnsTheTeamWithTheSmallestScoringDifference()
+        {
+            var footballService = new FootballService(new List<FootballStat>
+                {
+                    new FootballStat
+                    {
+                        Team="Team 1",
+                        For=50,
+                        Against=40
+                    },
+                    new FootballStat
+                    {
+                        Team="Team 2",
+                        For=50,
+                        Against=45
+                    }
+                });
+            var footballStat = footballService.GetTeamWithSmallestScoringDifference();
+
+            const int expectedTeam = "Team 2";
+            Assert.IsNotNull(footballStat);
+            Assert.AreEqual(expectedTeam, footballStat.Team);
+        }
+    }
+}
---
+++ Kata04.Football.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kata04.Football.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kata04.Football.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c4253d3b-f5c7-4724-b3d6-f2e2d130c351")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
