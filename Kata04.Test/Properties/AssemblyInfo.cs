787fb624651ee88db891b4e2dbf8d428fb6a7b6c
Aaron Chancey <achancey@quicksolutions.com>
2017-03-25 18:32:28 -0400

    Test for first kata part.

---
+++ Kata04.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kata04.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kata04.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7620c806-ea6b-432d-90bd-03e6aee444ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Kata04.Test/WeatherServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Kata04.Test
+{
+    [TestClass]
+    public class GivenAWeatherService
+    {
+        [TestMethod]
+        public void ThenGetDayWithSmallesstTempSpreadReturnsTheDayWithSmallestTempSpread()
+        {
+            var weatherServie = new WeatherService(new List<WeatherDay>
+                {
+                    new WeatherDay
+                    {
+                        Day=1,
+                        MaxTemp=50,
+                        MinTemp=40
+                    },
+                    new WeatherDay
+                    {
+                        Day=2,
+                        MaxTemp=50,
+                        MinTemp=45
+                    }
+                });
+            var weatherDay = weatherServie.GetDayWithSmallestTempSpread();
+
+            const int expectedDay = 2;
+            Assert.IsNotNull(weatherDay);
+            Assert.AreEqual(expectedDay, weatherDay.Day);
+        }
+    }
+}
